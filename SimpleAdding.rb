5cef139981b7fdfe1beeeae99acfce3677d0157b
Hanseul Yang <mir9559@gmail.com>
2015-07-31 13:43:23 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,9 @@
+def SimpleAdding(num)
+ 
+  return (1..num).reduce(:+)
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)    
